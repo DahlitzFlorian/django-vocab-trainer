c0dcc0a65810724c24737c7437e1eb43c5aaf7f9
florian dahlitz <f2dahlitz@freenet.de>
2017-04-19 23:06:48 +0200

    added basic index-page

--- vocabtrainer/urls.py
+++ vocabtrainer/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 import registration
+from . import views
 
 
 urlpatterns = [
+    url(r'$', views.index, name='index'),
     url(r'^vocab/', include('vocab.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
