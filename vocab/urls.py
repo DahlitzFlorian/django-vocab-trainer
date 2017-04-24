60d1dd324b047899908721a8dd67eca3553b28da
florian dahlitz <f2dahlitz@freenet.de>
2017-04-24 22:53:49 +0200

    set up basics for vocab-index

--- vocab/urls.py
+++ vocab/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 
 app_name = "vocab"
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name="index"),
+]
--- vocab/views.py
+++ vocab/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    """
+    Index-view for vocab-app
+    """
+    template_name = 'vocab/index.html'
+
+    context_object_name = 'sets'
+
+    def get_queryset(self):
+        """
+        Getting queryset for index-page of vocab-app
+        """
+        return []
