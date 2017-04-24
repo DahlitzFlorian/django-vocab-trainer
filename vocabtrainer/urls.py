32574c70bef70bef7a4e574cb7d49d83998b948a
florian dahlitz <f2dahlitz@freenet.de>
2017-04-24 22:49:27 +0200

    fixed url mistake

--- vocabtrainer/urls.py
+++ vocabtrainer/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls', namespace='accounts')),
     url(r'^admin/', admin.site.urls),
-    url(r'$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 ]
