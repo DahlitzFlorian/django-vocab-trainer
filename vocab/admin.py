950ef7d09b82142e4632ad15ba96397be9dfc2c0
florian dahlitz <f2dahlitz@freenet.de>
2017-04-25 10:29:43 +0200

    customized admin panel w/ classes

--- vocab/admin.py
+++ vocab/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 from .models import Learnset, Vocabulary
 
 
-admin.site.register(Learnset)
-admin.site.register(Vocabulary)
+class VocabularyInline(admin.TabularInline):
+    model = Vocabulary
+    extra = 3
+
+
+class LearnsetAdmin(admin.ModelAdmin):
+    # define inlines
+    inlines = [VocabularyInline]
+
+    # define search terms
+    search_fields = ['learnset_name']
+
+    # customize list displaying
+    list_display = ('learnset_name', 'creation_date')
+
+
+# register in admin panel
+admin.site.register(Learnset, LearnsetAdmin)
