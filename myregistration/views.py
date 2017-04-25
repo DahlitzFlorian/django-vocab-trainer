763e70599d87394a8dd17386a6e031823f77e5d5
florian dahlitz <f2dahlitz@freenet.de>
2017-04-25 11:08:07 +0200

    added user permissions

--- myregistration/views.py
+++ myregistration/views.py
@@ -16,8 +16,25 @@ class MyRegistrationView(RegistrationView):
             username=getattr(new_user, User.USERNAME_FIELD),
             password=form.cleaned_data['password1']
         )
+        # access to admin panel
         new_user.is_staff = True
+
+        # permissions
+        permission = Permission.objects.get(name="Can add learnset")
+        new_user.user_permissions.add(permission)
+        permission = Permission.objects.get(name="Can change learnset")
+        new_user.user_permissions.add(permission)
+        permission = Permission.objects.get(name="Can delete learnset")
+        new_user.user_permissions.add(permission)
+        permission = Permission.objects.get(name="Can add vocabulary")
+        new_user.user_permissions.add(permission)
+        permission = Permission.objects.get(name="Can change vocabulary")
+        new_user.user_permissions.add(permission)
+        permission = Permission.objects.get(name="Can delete vocabulary")
+        new_user.user_permissions.add(permission)
+
         new_user.save()
+        
         login(self.request, new_user)
         signals.user_registered.send(sender=self.__class__,
                                      user=new_user,
